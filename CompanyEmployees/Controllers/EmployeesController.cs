79cc1caf0399a544bf03e22d923b3225b0738132
Plamen Panayotov <plamen.panayotov@endava.com>
2022-04-18 17:28:16 +0300

    Implemented filtering

--- CompanyEmployees/Controllers/EmployeesController.cs
+++ CompanyEmployees/Controllers/EmployeesController.cs
@@ -32,6 +32,9 @@ namespace CompanyEmployees.Controllers
         public async Task<IActionResult> GetEmployeesForCompany(Guid companyId,
             [FromQuery] EmployeeParameters employeeParameters)
         {
+            if (!employeeParameters.ValidAgeRange)
+                return BadRequest("Max age can't be less than min age.");
+
             var company = await _repository.Company.GetCompanyAsync(companyId, false);
             if (company == null)
             {
