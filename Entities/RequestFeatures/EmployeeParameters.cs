278a5a5b31b06cc8a7c813021e929cc90bbbf35e
Plamen Panayotov <plamen.panayotov@endava.com>
2022-04-18 17:39:00 +0300

    Implemented search by name

--- Entities/RequestFeatures/EmployeeParameters.cs
+++ Entities/RequestFeatures/EmployeeParameters.cs
@@ -4,7 +4,7 @@
     {
         public uint MinAge { get; set; }
         public uint MaxAge { get; set; } = int.MaxValue;
-
         public bool ValidAgeRange => MaxAge > MinAge;
+        public string SearchTerm { get; set; }
     }
 }
